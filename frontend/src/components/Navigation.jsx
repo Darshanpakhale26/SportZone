fec6c15a0692682d813701935741745e67aa7e80
Amit Kumar Sinha <amitks1577@gmail.com>
2026-01-25 22:46:52 +0530

    Version 1.1

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useTheme } from '../context/ThemeContext';
 
 const Navigation = () => {
-  // Navigation Component
   const navigate = useNavigate();
   const { theme, toggleTheme } = useTheme();
   const user = JSON.parse(localStorage.getItem('user'));
